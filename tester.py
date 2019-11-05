45590db698b8fcceaedc6f02b72ac41cbd745f17
Mohammed Noman Razvi <47341161+noman55@users.noreply.github.com>
2019-11-05 13:28:25 +0530

    Updated

--- tester.py
+++ tester.py
@@ -1,9 +1,4 @@
-"""1. Getting Started with FCN Pre-trained Models
-==============================================
 
-This is a quick demo of using GluonCV FCN model on PASCAL VOC dataset.
-Please follow the `installation guide <../index.html>`_ to install MXNet and GluonCV if not yet.
-"""
 import mxnet as mx
 from mxnet import image
 from mxnet.gluon.data.vision import transforms
@@ -29,27 +24,3 @@ from gluoncv.utils.viz import get_color_pallete
 import matplotlib.image as mpimg
 mask = get_color_pallete(predict, 'pascal_voc')
 mask.save('output.png')
-
-
-
-##############################################################################
-# More Examples
-# -------------
-#
-#.. image:: https://raw.githubusercontent.com/dmlc/web-data/master/gluoncv/segmentation/voc_examples/4.jpg
-#    :width: 45%
-#
-#.. image:: https://raw.githubusercontent.com/dmlc/web-data/master/gluoncv/segmentation/voc_examples/4.png
-#    :width: 45%
-#
-#.. image:: https://raw.githubusercontent.com/dmlc/web-data/master/gluoncv/segmentation/voc_examples/5.jpg
-#    :width: 45%
-#
-#.. image:: https://raw.githubusercontent.com/dmlc/web-data/master/gluoncv/segmentation/voc_examples/5.png
-#    :width: 45%
-#
-#.. image:: https://raw.githubusercontent.com/dmlc/web-data/master/gluoncv/segmentation/voc_examples/6.jpg
-#    :width: 45%
-#
-#.. image:: https://raw.githubusercontent.com/dmlc/web-data/master/gluoncv/segmentation/voc_examples/6.png
-#    :width: 45%
